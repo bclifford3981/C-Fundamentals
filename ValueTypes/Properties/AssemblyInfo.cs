bc13c110b3fc6b524f4229e03ed5d6226889fb58
bclifford23 <73857749+bclifford23@users.noreply.github.com>
2021-03-09 14:58:58 -0500

    added value types

---
+++ ValueTypes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValueTypes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValueTypes")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9aef4c6f-6513-412e-b146-ad11fe33acf5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ValueTypes/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ValueTypes
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Booleans()
+        {
+            bool isDeclared;
+            isDeclared = true;
+
+            bool isDeclaredAndInitilized = false;
+
+            isDeclaredAndInitilized = true;
+
+        }
+        [TestClass]
+        public class Characters()
+        {
+        char character = 'a';
+        char symbol = '?';
+        char numbers = '7';
+        char space = ' ';
+        char specialCharacter = '\n';
+
+        [TestMethod]
+        public void Decimals()
+        {
+            float floatExample = 1.045231f;
+            double doubleExample = 1.789053278907036d;
+            decimal decimalExample = 1.2578907289045789789789789787897m;
+        }
+        [TestMethod]
+
+        public void WholeNumbers();
+        {
+         byte byteExample = 255;
+        sbyte sByteExample = 127;
+        short shortExample = 32767;
+        Int16 anotherShortExample = 32000;
+        int intmin = -2147483647;
+        Int32 intMax = 2147483647;
+        long longExample = 9223372036854775807;
+        Int64 longMin = -9223372036854775807;
+
+        int a = 15;
+        int b = -15;
+    }
+    }
+}
+
+
+
+
